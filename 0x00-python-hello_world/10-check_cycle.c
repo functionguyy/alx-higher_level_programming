548a7460ec0358a5387e5db555d5c5eae1a3e9cb
Gilbert Adikankwu <gilbert945@outlook.com>
2023-03-07 02:47:19 +0100

    feat: Add C function

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,7 +3,7 @@
 /**
 * check_cycle - checks if a singly linked list has a cycle in it
 * @list: a pointer to the list
-* 
+*
 * Return: 0 if there is no cycle or 1 if there is a cycle
 */
 
@@ -12,7 +12,7 @@ int check_cycle(listint_t *list)
 	listint_t *main = list;
 	listint_t *check = list;
 
-	
+
 	if (list == NULL || list->next)
 	{
 		return (0);
