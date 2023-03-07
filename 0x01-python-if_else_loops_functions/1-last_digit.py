aa7a8eb54acf9c277e248a04544dbbecd207b0b2
Gilbert Adikankwu <gilbert945@outlook.com>
2023-03-07 20:32:01 +0100

    fix: Correct code to get proper output

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,12 +6,13 @@ sign = 1
 if number < 0:
     sign *= -1
 
-last_digit = (number % 10) * sign
-first_str = "Last digit of {0:d} is {1:d} and is greater than {2:d}"
-second_str = "Last digit of {0:d} is {1:d} and is less than {2:d} and not {3:d}"
+last_digit = (abs(number) % 10) * sign
+first_str = "Last digit of {:d} is {:d} and is greater than {:d}"
+second_str = "Last digit of {:d} is {:d} and is less than {:d} and not {:d}"
+third_str = "Last digit of {:d} is {:d} and is {:d}"
 if last_digit > 5:
     print(first_str.format(number, last_digit, 5))
 elif last_digit < 6 and last_digit != 0:
     print(second_str.format(number, last_digit, 6, 0))
 else:
-    print("Last digit of {:d} is {:d}".format(number, last_digit))
+    print(third_str.format(number, last_digit, 0))
