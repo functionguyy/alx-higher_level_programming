c13a70fa1500b750c87fde1fe1ea7bc6385b86ff
Gilbert Adikankwu <gilbert945@outlook.com>
2023-03-08 08:47:01 +0100

    fix: Remove unnecessary check condition

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    sign = 1
-    if number < 0:
-        sign *= -1
 
     last_digit = (abs(number) % 10) * sign
     print("{:d}".format(last_digit), end="")
