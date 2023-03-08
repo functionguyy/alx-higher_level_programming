7abe27dbce939b7fa73c637f6f8b16a1089aa2a5
Gilbert Adikankwu <gilbert945@outlook.com>
2023-03-08 08:49:31 +0100

    fix: Remove unused variable

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 
 def print_last_digit(number):
 
-    last_digit = (abs(number) % 10) * sign
+    last_digit = (abs(number) % 10)
     print("{:d}".format(last_digit), end="")
 
     return last_digit
