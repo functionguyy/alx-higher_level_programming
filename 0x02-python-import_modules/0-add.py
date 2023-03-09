ffa945f44850de66054368c8949053a5159f680e
Gilbert Adikankwu <gilbert945@outlook.com>
2023-03-09 20:41:33 +0100

    fix: Correct the code structure

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import sys
 from add_0 import add
 
 
@@ -7,8 +6,7 @@ def my_function():
     a = 1
     b = 2
     print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
-    return 0
 
 
 if __name__ == '__main__':
-    sys.exit(my_function())
+    my_function()
