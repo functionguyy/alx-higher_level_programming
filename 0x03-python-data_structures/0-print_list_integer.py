9dbe6fbe6fc3cd6e473222dd2325bbe437cdacf0
Gilbert Adikankwu <gilbert945@outlook.com>
2023-03-13 22:40:53 +0100

    fix: Add blank line

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -9,6 +9,7 @@ def print_list_integer(my_list: list = []) -> None:
     return: print one integer per line
 
     """
+
     if len(my_list) > 0:
         for num in my_list:
             print("{:d}".format(num))
