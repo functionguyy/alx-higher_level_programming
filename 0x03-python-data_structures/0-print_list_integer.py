6dae4849cd1f06485bdb13e4e4e49c6f16527e50
Gilbert Adikankwu <gilbert945@outlook.com>
2023-03-13 22:35:17 +0100

    feat: Add a function that prints all integers of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list: list = []) -> None:
+    """A function that prints all integers of a list
+
+    args:
+    my_list: list object
+
+    return: print one integer per line
+
+    """
+    for num in my_list:
+        print("{:d}".format(num))
