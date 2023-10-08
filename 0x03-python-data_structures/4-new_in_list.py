92fb3c01ed52349d867c6704c86aa8a9f37cfa67
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-10-08 08:03:57 +0100

    fix: add assignment operator to assign list len to n

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def new_in_list(my_list: list, idx: int, element) -> list:
     """Function that replaces an element in a list at a specific position
     without modifying the original list
@@ -14,7 +13,7 @@ def new_in_list(my_list: list, idx: int, element) -> list:
 
     """
     if type(my_list) is list and type(idx) is int:
-        n len(my_list)
+        n = len(my_list)
 
         if idx < 0 or idx >= n:
             return my_list
