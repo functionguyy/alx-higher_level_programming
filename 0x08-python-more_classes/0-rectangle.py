9e5e45815f6ce709d7ddb6da5e6799aa5df75d10
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-10-30 16:51:44 +0100

    feat: define an empty class for a Rectangle type

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a Rectangle object type"""
+
+
+class Rectangle:
+    """Define the internals of a Rectangle object type"""
+    def __init__(self):
+        """Initialize an instance of a Rectangle object type"""
