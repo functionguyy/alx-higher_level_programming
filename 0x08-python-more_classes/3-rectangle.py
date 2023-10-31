d307a6a1f61442575a446eed1d67aeaf3a47e9ff
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-10-31 10:54:47 +0100

    feat: add __str__ magic method to Rectangle object

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""defines a Rectangle object type"""
+
+
+class Rectangle:
+    """Define the internals of a Rectangle object type"""
+    def __init__(self, width=0, height=0):
+        """Initialize an instance of a Rectangle object type"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return area of a rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """return perimeter of a rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.height + self.width)
+
+    def __str__(self):
+        rec = ""
+        if self.width == 0 or self.height == 0:
+            return rec
+        else:
+            for i in range(self.height):
+                rec += "#" * self.width + "\n"
+            return rec[:-1]
