2d63a5e042571ff22df967f1705efe5e5372a8b6
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-11-06 19:58:08 +0100

    fix: make file executable

--- 0x0A-python-inheritanc/0-lookup.py
+++ 0x0A-python-inheritanc/0-lookup.py
@@ -1,6 +1,12 @@
+#!/usr/bin/python3
 """Module defines function lookup"""
 
 
-def lookup(obj) -> list:
-    """returns list of attributes and methood of an object"""
+def lookup(obj):
+    """returns list of attributes and methood of an object
+
+    Attributes:
+        obj (:obj): object
+
+    """
     return dir(obj)
