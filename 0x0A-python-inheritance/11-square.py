7450f7a966c38150458fb141cd967a8a18dd14bb
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-11-07 12:52:39 +0100

    feat: add __str__  method to class Square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module defines class Square"""
+rectangle = __import__('9-rectangle')
+
+
+class Square(rectangle.Rectangle):
+    """Define the internals of a Square object type.
+
+    Square is a subclass of Rectangle
+    """
+
+    def __init__(self, size):
+        """initialize an instance of Square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """calculate the area of a Square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """string representation of Square"""
+        return "[Square] {0:d}/{0:d}".format(self.__size)
