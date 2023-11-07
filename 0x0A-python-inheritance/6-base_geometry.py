75ff0f742dcd8cb6f49cbc03c062134d2ea2a6b5
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-11-07 07:53:13 +0100

    feat: improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module defines class BaseGeometry"""
+
+
+class BaseGeometry:
+    """empty class"""
+
+    def __init__(self):
+        """intialize an instance of BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
