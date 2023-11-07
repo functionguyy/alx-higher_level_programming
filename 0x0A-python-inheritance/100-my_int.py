3028a31d1097f04f9a02dc5b00cf28277afb8f85
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-11-07 16:04:00 +0100

    feat: add class that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module defines class MyInt
+MyInt is a subclass of int
+"""
+
+
+class MyInt(int):
+    """Defines the internal representation of a MyInt object type"""
+
+    def __init__(self, value):
+        """initialize an instance of MyInt"""
+        self.value = value
+
+
+    def __ne__(self, number):
+        """return self == number"""
+        return self.value == number
+
+    def __eq__(self, number):
+        """return self != number"""
+        return self.value != number
+
