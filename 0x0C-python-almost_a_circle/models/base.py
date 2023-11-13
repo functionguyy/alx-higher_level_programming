20454ce34be3d21ef623cde233fbb98932b5bca2
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-11-13 13:13:23 +0100

    fix: add guard against assigning negative id

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,5 +9,7 @@ class Base:
     def __init__(self, id=None):
         if id is None:
             self.id = type(self).__nb_object + 1
+        elif id < 0:
+            raise ValueError("id must be a positive integer")
         else:
             self.id = id
