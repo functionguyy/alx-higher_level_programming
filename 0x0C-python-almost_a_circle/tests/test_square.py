8e0983bf267abe426ef6a2e6693888d6efcabd1f
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-11-13 16:26:42 +0100

    feat: test case for instantiation attributes

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -2,6 +2,8 @@
 import unittest
 from models.rectangle import Rectangle
 from models.square import Square
+from io import StringIO
+from unittest.mock import patch
 
 
 class InheritanceTestCase(unittest.TestCase):
@@ -9,6 +11,98 @@ class InheritanceTestCase(unittest.TestCase):
     def test_square_object_is_instance_of_rectangle(self):
         a = Square(5)
         self.assertIsInstance(a, Rectangle)
+
+
+class IdAttrTestCase(unittest.TestCase):
+
+    def test_id_is_not_none(self):
+        """Square object id should not be None"""
+        a = Square(5)
+        self.assertIsNotNone(a.id)
+
+    def test_id_specified(self):
+        """Square object id should be the id passed if any"""
+        a = Square(9, 15, id=3)
+        self.assertEqual(a.id, 3)
+
+    def test_negative_id_input(self):
+        """assigning to id should fail with negative input"""
+        with self.assertRaises(ValueError):
+            a = Square(5, id=-2)
+
+
+class SizeAttrTestCase(unittest.TestCase):
+
+    def test_string_input(self):
+        """assigning to size should fail with string input"""
+        with self.assertRaises(TypeError):
+            a = Square("hello")
+
+    def test_negative_input(self):
+        """assigning to size should fail with negative integer input"""
+        with self.assertRaises(ValueError):
+            a = Square(-9)
+
+    def test_float_input(self):
+        """assigning to size should fail with float input"""
+        with self.assertRaises(TypeError):
+            a = Square(1.1)
+
+    def test_bool_input(self):
+        """assigning to size should fail with bool input"""
+        with self.assertRaises(TypeError):
+            a = Square(True)
+
+    def test_zero_input(self):
+        """assigning to size should fail with zero input"""
+        with self.assertRaises(ValueError):
+            a = Square(0)
+
+
+class XAttrTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.square = Square(5)
+
+    def test_string_input(self):
+        """assigning to x should fail with string input"""
+        with self.assertRaises(TypeError):
+            self.square.x = "test"
+
+    def test_negative_input(self):
+        """assigning to x should fail with string input"""
+        with self.assertRaises(ValueError):
+            self.square.x = -5
+
+
+class YAttrTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.square = Square(5)
+
+    def test_string_input(self):
+        """assigning to y should fail with string input"""
+        with self.assertRaises(TypeError):
+            self.square.y = "new"
+
+    def test_negative_input(self):
+        """assigning to y should fail with negative input"""
+        with self.assertRaises(ValueError):
+            self.square.y = -3
+
+
+class StrAttrTestCase(unittest.TestCase):
+
+    def test_str_attr_output(self):
+        s = Square(5, 2, 3, 89)
+        out = "[Square] (89) 2/3 - 5"
+
+        with patch('sys.stdout', new=StringIO()) as output:
+            print(s)
+            print_out = output.getvalue().strip()
+            self.assertEqual(print_out, out)
 
 
 if __name__ == "__main__":
