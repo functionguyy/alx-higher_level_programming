b8adefddb0d999a087f9b163446a592d2e3c9718
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-11-14 00:10:17 +0100

    fix: remove check for negative id

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,7 +7,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is None or id < 0 :
+        if id is None:
             self.id = type(self).__nb_objects + 1
         else:
             self.id = id
