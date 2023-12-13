b7d20c049a5dda2401219da3abac2770b4d3e275
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-12-13 11:59:23 +0100

    fix: write comment with the right syntax

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
---creates a table called first_table in the current database in your MySQL
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT, 
-	name VARCHAR(256)
-);
+-- creates a table called first_table in the current database in your MySQL
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
