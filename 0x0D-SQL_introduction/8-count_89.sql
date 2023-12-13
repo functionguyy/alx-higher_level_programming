9727ade1610dc48d2e88ff2353b52401174c0e5e
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-12-13 14:02:08 +0100

    feat: display the number of records with id = 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- display number of records with id = 89 in the table first_table
+-- of database hbtn_0c_0
+-- The database name will be passed as an argument of mysql command
+SELECT COUNT(id) FROM first_table WHERE id = 89;
