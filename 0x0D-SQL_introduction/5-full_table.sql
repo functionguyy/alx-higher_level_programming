e35725e62d3eed32783060eab40e216b2ca80ffa
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2023-12-13 12:51:52 +0100

    feat: show the create statement of a table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,5 @@
--- print full description
-SHOW FULL COLUMNS FROM first_table
+-- print full description of the table first_table from
+-- the database hbtn_0c_0
+-- database argument is passed as an argument of the mysql command
+-- show the create statement that creates first_table
+SHOW CREATE TABLE first_table
