ae1324f928bd98d495f2f6b72a169fdf57b01073
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-01-09 02:44:01 +0100

    feat: print 3 lines of phrases

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const phrases = {
+  firstPhrase: 'C is fun',
+  secondPhrase: 'Python is cool',
+  thirdPhrase: 'Javascript is amazing'
+};
+
+const { firstPhrase, secondPhrase, thirdPhrase } = phrases;
+
+console.log(firstPhrase);
+console.log(secondPhrase);
+console.log(thirdPhrase);
