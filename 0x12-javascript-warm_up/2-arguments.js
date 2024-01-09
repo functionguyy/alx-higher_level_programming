ad656de07b5a5e5b8fcb08211f0caeef6bb7ced7
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-01-09 03:42:51 +0100

    fix; use variables in conditional block

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,14 +2,15 @@
 
 const { argv } = require('node:process');
 
-if (argv.length - 2 === 0) {
-  console.log('No argument');
-}
+const argCount = argv.length - 2;
+const noArg = 'No argument';
+const oneArg = 'Argument found';
+const moreArg = 'Arguments found';
 
-if (argv.length - 2 === 1) {
-  console.log('Argument found');
-}
-
-if (argv.length - 2 > 1) {
-  console.log('Arguments found');
+if (argCount === 0) {
+  console.log(noArg);
+} else if (argCount === 1) {
+  console.log(oneArg);
+} else {
+  console.log(moreArg);
 }
