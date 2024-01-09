8842e41f1a6f0cc0b88cf6a363090f4175ebfde3
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-01-09 02:57:05 +0100

    fix: assign correct value to thirdPhrase

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,7 @@
 
 const firstPhrase = 'C is fun';
 const secondPhrase = 'Python is cool';
-const thirdPhrase = 'Javascript is amazing';
+const thirdPhrase = 'JavaScript is amazing';
 
 console.log(firstPhrase);
 console.log(secondPhrase);
