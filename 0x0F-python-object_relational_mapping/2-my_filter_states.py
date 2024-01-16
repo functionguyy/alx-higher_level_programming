16e046f9358af8b2fed597e75147d206ce0a818f
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-01-16 22:22:07 +0100

    feat: display values in states table that match

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" script that lists all states with names starting with uppercase N from the
+database hbtn_0e_0_usa
+"""
+import argparse
+import MySQLdb
+
+
+def query_db():
+    parser = argparse.ArgumentParser(prog="MySQL_login")
+    parser.add_argument('username')
+    parser.add_argument('password')
+    parser.add_argument('db_name')
+    parser.add_argument('state_name')
+    args = parser.parse_args()
+
+    db_conn = MySQLdb.connect(host="localhost",
+                              port=3306,
+                              user=args.username,
+                              passwd=args.password,
+                              db=args.db_name, charset="utf8")
+    db_cur = db_conn.cursor()
+    db_cur.execute(
+            """SELECT * FROM states
+            WHERE name = %s
+            ORDER BY id ASC""", (args.state_name,))
+    query_rows = db_cur.fetchall()
+    for row in query_rows:
+        print(row)
+    db_cur.close()
+    db_conn.close()
+
+
+if __name__ == '__main__':
+    query_db()
