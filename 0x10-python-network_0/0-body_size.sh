b05320a16c5a3ccbb92ed31304c8c5b3887525bb
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-01-28 07:46:50 +0100

    fix: reduce line count to 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
-
-if [ "$#" -eq "1" ]
-then
-	body_size=$(curl -s -w '%{size_download}' -o /dev/null "$1")
-else
-	echo "Usage: $(basename "$0") <IP addr>:<port>"
-	exit 1
-fi
-
-echo "$body_size"
+# curling the server then filtering to only get the number
+echo "$(curl -s -w '%{size_download}' -o /dev/null "$1")"
