5d7574286fbcfea53e42614f6ce69c890c0e6c40
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-04-07 14:11:59 +0100

    feat: add relationship attribute to State class

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module contains the class definition of a State class and SQLAlchemy
+declarative base class"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class mapped to states table"""
+
+    __tablename__ = 'states'
+
+    id = Column(Integer,
+                primary_key=True,
+                unique=True,
+                nullable=False,
+                )
+    name = Column(String(128),
+                  nullable=False)
+    cities = relationship("City", back_ref="state")
