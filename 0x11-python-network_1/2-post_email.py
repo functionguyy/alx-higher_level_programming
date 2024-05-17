9ae42f213213ca42827fc9278af832283f8c99ae
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-05-17 12:28:51 +0100

    feat: send POST request with email as parameter

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script takes in a URL and an email,sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the response"""
+import urllib.request as url_req
+import urllib.parse as url_parse
+import sys
+
+
+def fetch_url(url, data):
+    """fetch a url with data"""
+
+    # send POST request
+    req = url_req.Request(url, data)
+    with url_req.urlopen(req) as response:
+        the_page = response.read()
+
+    decoded_content = the_page.decode('utf-8')
+
+    return decoded_content
+
+
+if __name__ = "__main__":
+    # prepare parameters for POST request
+    values = {'email': sys.argv[2]}
+    data = url_parse.urlencode(values)
+    data = data.encode('ascii')
+
+    # fetch url response object
+    response_content = fetch_url(sys.argv[1], data)
+
+    print(response_content)
