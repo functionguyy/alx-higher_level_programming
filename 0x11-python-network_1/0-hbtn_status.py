942cbd99f71d98fe91a877f7ee8a9dc82646a69a
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-05-17 08:47:37 +0100

    feat: add script that fetches a url using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request as url_req
+
+req = url_req.Request('https://alx-intranet.hbtn.io/status')
+with url_req.urlopen(req) as response:
+    the_page = response.read()
+
+decoded_content = the_page.decode('utf-8')
+print('\t- type:', type(the_page))
+print('\t- content:', the_page)
+print('\t- utf8 content:', decoded_content)
