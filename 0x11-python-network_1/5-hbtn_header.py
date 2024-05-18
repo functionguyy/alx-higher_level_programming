cd02c078e340c662ee1f28bba3278584b1ad79d6
Gilbert Adikankwu <gilbertadikankwu@gmail.com>
2024-05-18 12:24:08 +0100

    feat: get URL content and print X-Request-Id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script takes in a URL, sends a request to the URL and displays the value of
+the variable X-Request-Id in the response header
+"""
+import sys
+import requests
+
+
+def fetch_url(url):
+    """get response from server"""
+
+    r = requests.get(url)
+
+    return r
+
+
+if __name__ == "__main__":
+    response = fetch_url(sys.argv[1])
+    print(response.headers['X-Request-Id'])
